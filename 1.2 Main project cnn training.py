3f904a826d5f5e0e0561414d9f9640881891501c
nihad abdulla <nihadabdulla232@gmail.com>
2023-04-18 00:12:58 +0530

    training and prediction

--- 1.2 Main project cnn training.py
+++ 1.2 Main project cnn training.py
@@ -1,430 +1,96 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[29]:
+# In[23]:
+
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Define the input shape of the images and the number of classes
+input_shape = (128, 128, 1)
+num_classes = 53   # English alphabet has 26 letters
+
+# Define the CNN model architecture
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes, activation='softmax')
+])
+
+# Compile the model with an optimizer, loss function, and evaluation metric
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Load the training and testing data using ImageDataGenerator
+train_data = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+train_generator = train_data.flow_from_directory('C:/input/train', target_size=input_shape[:2],
+                                                  color_mode='grayscale', batch_size=32, shuffle=True,
+                                                  class_mode='categorical', subset='training')
+val_generator = train_data.flow_from_directory('C:/input/test', target_size=input_shape[:2],
+                                                color_mode='grayscale', batch_size=32, shuffle=True,
+                                                class_mode='categorical')
+print("Training started...")
+# Train the model
+history = model.fit(train_generator, epochs=50, validation_data=val_generator)
+print("Training completed.")
+
+# Save the trained model
+model.save('C:/input/models/model2/modelfull1-2.h5')
+
+# Print the accuracy and loss of the model on the validation set
+val_loss, val_acc = model.evaluate(val_generator)
+print("Validation accuracy:", val_acc)
+print("Validation loss:", val_loss)
+
+
+# In[8]:
 
 
 import os
+from tensorflow.keras.preprocessing import image
 import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from keras.optimizers import Adam
-
-# Set the batch size, number of epochs, and target image size
-batch_size = 5
-num_epochs = 10
-target_size = (224, 224)
-
-# Set the directory paths for training, validation, and testing
-train_dir = 'C:/input/training'
-valid_dir = 'C:/input/validation'
-test_dir = 'C:/input/testing'
-
-# Set the label for the character
-char_label = 'ന'
-
-# Create the data generators for training, validation, and testing
-train_datagen = ImageDataGenerator(rescale=1./255)
-valid_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-train_generator = train_datagen.flow_from_directory(
-    train_dir,
-    target_size=target_size,
-    color_mode='rgb',
-    batch_size=batch_size,
-    class_mode='binary')
-
-valid_generator = valid_datagen.flow_from_directory(
-    valid_dir,
-    target_size=target_size,
-    color_mode='rgb',
-    batch_size=batch_size,
-    class_mode='binary')
-
-test_generator = test_datagen.flow_from_directory(
-    test_dir,
-    target_size=target_size,
-    color_mode='rgb',
-    batch_size=batch_size,
-    class_mode='binary')
-
-print("Number of training samples:", train_generator.n)
-print("Number of validation samples:", valid_generator.n)
-print("Number of testing samples:", test_generator.n)
-
-
-# Build the CNN model architecture
-model = Sequential()
-
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Flatten())
-
-model.add(Dense(64, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-model.summary()
-
-# Compile the model with Adam optimizer and binary crossentropy loss
-model.compile(optimizer=Adam(learning_rate=0.0001),
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-# Train the model using the data generator for training and validate on the validation set
-print('Training the model...')
-history = model.fit(
-    train_generator,
-    steps_per_epoch=len(train_generator),
-    epochs=num_epochs,
-    validation_data=valid_generator,
-    validation_steps=len(valid_generator),
-    verbose=1)
-
-# Evaluate the model on the testing set
-print('Evaluating the model...')
-test_loss, test_acc = model.evaluate(
-    test_generator, 
-    steps=len(test_generator),
-    verbose=1)
-
-print('Test accuracy:', test_acc)
-
-# Save the model with the character label as the filename
-model.save('C:/input/models/model1/model.h5')
-
-print('Model for character ' + char_label + ' saved to C:/input/models/model1.')
-
-
-# In[2]:
-
-
-import os
-import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from keras.optimizers import Adam
-
-# Set the batch size, number of epochs, and target image size
-batch_size = 5
-num_epochs = 10
-target_size = (224, 224)
-
-# Set the directory paths for training, validation, and testing
-train_dir = 'C:/input/training'
-valid_dir = 'C:/input/validation'
-test_dir = 'C:/input/testing'
-
-# Set the label for the character
-char_label = 'ന'
-
-# Create the data generators for training, validation, and testing
-train_datagen = ImageDataGenerator(rescale=1./255)
-valid_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-train_generator = train_datagen.flow_from_directory(
-    train_dir,
-    target_size=target_size,
-    color_mode='grayscale',
-    batch_size=batch_size,
-    class_mode='binary')
-
-valid_generator = valid_datagen.flow_from_directory(
-    valid_dir,
-    target_size=target_size,
-    color_mode='grayscale',
-    batch_size=batch_size,
-    class_mode='binary')
-
-test_generator = test_datagen.flow_from_directory(
-    test_dir,
-    target_size=target_size,
-    color_mode='grayscale',
-    batch_size=batch_size,
-    class_mode='binary')
-
-print("Number of training samples:", train_generator.n)
-print("Number of validation samples:", valid_generator.n)
-print("Number of testing samples:", test_generator.n)
-
-
-# Build the CNN model architecture
-model = Sequential()
-
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 1)))
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Flatten())
-
-model.add(Dense(64, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-model.summary()
-
-# Compile the model with Adam optimizer and binary crossentropy loss
-model.compile(optimizer=Adam(learning_rate=0.0001),
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-# Train the model using the data generator for training and validate on the validation set
-print('Training the model...')
-history = model.fit(
-    train_generator,
-    steps_per_epoch=len(train_generator),
-    epochs=num_epochs,
-    validation_data=valid_generator,
-    validation_steps=len(valid_generator),
-    verbose=1)
-
-# Evaluate the model on the testing set
-print('Evaluating the model...')
-test_loss, test_acc = model.evaluate(
-    test_generator, 
-    steps=len(test_generator),
-    verbose=1)
-
-print('Test accuracy:', test_acc)
-
-# Save the model with the character label as the filename
-model.save('C:/input/models/model1/model.h5')
-
-print('Model for character ' + char_label + ' saved to C:/input/models/model1.')
-
-
-
-# In[72]:
-
-
-import os
-import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, BatchNormalization, Dropout
-from keras.optimizers import Adam
-from sklearn.metrics import classification_report, confusion_matrix
-
-# Set the batch size, number of epochs, and target image size
-batch_size = 5
-num_epochs = 10
-target_size = (224, 224)
-
-# Set the directory paths for training, validation, and testing
-train_dir = 'C:/input/training'
-valid_dir = 'C:/input/validation'
-test_dir = 'C:/input/testing'
-
-# Set the label for the character
-char_label = 'ന'
-
-# Create the data generators for training, validation, and testing
-train_datagen = ImageDataGenerator(rescale=1./255)
-valid_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-train_generator = train_datagen.flow_from_directory(
-    train_dir,
-    target_size=target_size,
-    color_mode='rgb',
-    batch_size=batch_size,
-    class_mode='binary')
-
-valid_generator = valid_datagen.flow_from_directory(
-    valid_dir,
-    target_size=target_size,
-    color_mode='rgb',
-    batch_size=batch_size,
-    class_mode='binary')
-
-test_generator = test_datagen.flow_from_directory(
-    test_dir,
-    target_size=target_size,
-    color_mode='rgb',
-    batch_size=batch_size,
-    class_mode='binary')
-
-print("Number of training samples:", train_generator.n)
-print("Number of validation samples:", valid_generator.n)
-print("Number of testing samples:", test_generator.n)
-
-
-# Build the CNN model architecture
-model = Sequential()
-
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
-model.add(BatchNormalization())
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Conv2D(256, (3, 3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Flatten())
-
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
-
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-
-model.add(Dense(1, activation='sigmoid'))
-
-model.summary()
-
-
-# Compile the model with Adam optimizer and binary crossentropy loss
-model.compile(optimizer=Adam(learning_rate=0.0001),
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-# Train the model using the data generator for training and validate on the validation set
-print('Training the model...')
-history = model.fit(
-    train_generator,
-    steps_per_epoch=len(train_generator),
-    epochs=num_epochs,
-    validation_data=valid_generator,
-    validation_steps=len(valid_generator),
-    verbose=1)
-
-# Evaluate the model on the testing set
-print('Evaluating the model...')
-test_loss, test_acc = model.evaluate(
-    test_generator, 
-    steps=len(test_generator),
-    verbose=1)
-
-print('Test accuracy:', test_acc)
-
-# Get the predicted and true labels for the test set
-Y_pred = model.predict(test_generator)
-y_pred = np.argmax(Y_pred, axis=1)
-y_true = test_generator.classes
-
-# Print the classification report and confusion matrix
-print('Classification Report')
-print(classification_report(y_true, y_pred))
-
-print('Confusion Matrix')
-print(confusion_matrix(y_true, y_pred))
-
-# Save the model with the character label as the filename
-model.save('C:/input/models/model2/model.h5')
-
-import matplotlib.pyplot as plt
-
-# Plot the training and validation loss over the number of epochs
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs = range(1, len(loss) + 1)
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
-
-
-# In[10]:
-
-
-from PIL import Image
-
-# Open the image and get its mode
-img = Image.open('C:/input/character segmentation/e1char3.png')
-mode = img.mode
-
-# Print the mode to determine the number of channels
-print(mode)
-
-
-# In[ ]:
-
-
-
-
-
-# In[64]:
-
-
-# Load the saved model
-saved_model = load_model('C:/input/models/model2/model.h5')
-
-# Load the image and preprocess it
-img_path = 'C:\input\character segmentation\IMG187.jpg'
-img = cv2.imread(img_path)
-img = cv2.resize(img, target_size)
-img = np.reshape(img, (1, 224, 224, 3))
-
-# Make a prediction
-prediction = saved_model.predict(img)
-
-# Extract the label from the saved model
-label_map = train_generator.class_indices
-label_map = dict((v,k) for k,v in label_map.items())
-label = label_map[int(prediction[0][0])]
-
-print('The predicted label is:', label)
-
-
-# In[76]:
-
-
-import os
-import numpy as np
-import cv2
-from keras.models import load_model
-
-# Set the target image size
-target_size = (224, 224)
-
-# Load the saved model
-saved_model = load_model('C:/input/models/model2/model.h5')
-
-# Define the character label
-char_label = saved_model.output_names[0]
-
-# Load the test image
-img_path = 'C:\input\character segmentation\IMG187.jpg'
-img = cv2.imread(img_path)
-img = cv2.resize(img, target_size)
-img = np.reshape(img, (1, 224, 224, 3))
-
-# Make a prediction on the image using the saved model
-prediction = saved_model.predict(img)
-
-# Get the label of the prediction
-if prediction[0][0] > 0.5:
-    label = char_label
-else:
-    label = 'Not ' + char_label
-
-# Print the label of the prediction
-print('Prediction:', label)
-
-
-# In[ ]:
-
-
-
+import tensorflow as tf
+
+# Load the trained model
+model_path = 'C:/input/models/model2/modelfull1-2.h5'
+model = tf.keras.models.load_model(model_path)
+
+# Load the label mapping from the training data generator
+training_data_dir = 'C:/input/train'
+train_datagen = image.ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(training_data_dir, target_size=(128, 128), color_mode='grayscale', batch_size=32, class_mode='categorical')
+label_map = dict((v,k) for k,v in train_generator.class_indices.items())
+
+# Predict images in a folder
+image_folder_path = 'C:/input/character segmentation/'
+predicted_labels = []
+for filename in os.listdir(image_folder_path):
+    if filename.endswith('.png') or filename.endswith('.jpg'):
+        # Load and preprocess the image
+        image_path = os.path.join(image_folder_path, filename)
+        img = image.load_img(image_path, color_mode='grayscale', target_size=(128, 128))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0
+        
+        # Make a prediction
+        prediction = model.predict(img_array,verbose=0)
+        
+        # Get the predicted label
+        predicted_label = np.argmax(prediction, axis=1)[0]
+        
+        # Store the predicted label
+        predicted_labels.append(predicted_label)
+
+# Print the predicted labels and their corresponding class names in a single line
+predicted_classes = [label_map[label] for label in predicted_labels]
+print('Predicted classes:', ''.join(predicted_classes))
+with open('C:/input/validation/predicted_classes.txt', 'w', encoding='utf-8') as f:
+    f.write(''.join(predicted_classes))
 
 
 # In[ ]:
