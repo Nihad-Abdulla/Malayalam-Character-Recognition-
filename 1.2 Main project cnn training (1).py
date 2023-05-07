dc81fee4bd6c73b013a7034584770b0909fb2b75
nihad abdulla <nihadabdulla232@gmail.com>
2023-05-08 02:42:32 +0530

    updated

--- 1.2 Main project cnn training (1).py
+++ 1.2 Main project cnn training (1).py
@@ -10,7 +10,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.optimizers import Adam
-
+# Set the batch size, number of epochs, and target image size
 # Set the batch size, number of epochs, and target image size
 batch_size = 5
 num_epochs = 10
